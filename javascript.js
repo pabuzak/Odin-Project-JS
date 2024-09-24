35b96ea163484328ec3fb1b78037674503ad0388
pabuzak <pabuzak@gmail.com>
2024-09-24 20:16:05 +1200

    end of day 1

---
+++ javascript.js
@@ -0,0 +1,35 @@
+// let name = "John";
+// let surname = "Doe";
+
+// console.log(name);
+// console.log(surname);
+
+// let age = 11;
+// console.log(age);
+
+// age = 54;
+
+// console.log(age);
+
+// const pi = 3.14;
+// // pi = 10;
+
+// console.log(pi);
+
+// assignment
+console.log(23 + 97);
+
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+console.log((4 + 6 + 9)/77);
+
+let a = 10;
+console.log(a);
+console.log(a*9);
+
+let b = 7 * a;
+console.log(b);
+
+let max = 57;
+let actual = max-13;
+let percentage = actual/max;
